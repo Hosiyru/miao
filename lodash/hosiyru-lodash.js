88ffd23ae156e8dd3a90014363da19eabb2beb14
Hosiyru <995323494@qq.com>
2023-06-05 10:04:33 +0800

    6-5

---
+++ lodash/hosiyru-lodash.js
@@ -0,0 +1,31 @@
+var hosiyru = {
+  chunk: (array, size) => {
+    let result = []
+    for (let i = 0; i < array.length; i += size) {
+      let h = []
+      for (let j = 0; j < size; j++) {
+        if (!array[i + j]) break
+        h.push(array[j])
+      }
+      result.push(h)
+    }
+  },
+  
+  compact: (array) => {
+    let result = [], i = 0
+    while(i < array.length) {
+      if (!(array[i] == false || array[i] == null || array[i] == 0 || array[i] == '' || array[i] == undefined || array[i] == NaN)) result.push(array[i])
+      i++
+    }
+    return result
+  },
+
+  concat: (array, ...values) => {
+    let result = array
+    for (let item of values) {
+      if (Array.isArray(item)) result.push(...item)
+      else result.push(item)
+    }
+    return result
+  }
+}
